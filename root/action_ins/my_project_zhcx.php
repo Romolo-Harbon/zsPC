9955f69a3d8ef519bbb1a09078cad04a77b6bd01
Lin971018 <30288831+Lin971018@users.noreply.github.com>
2018-10-09 18:31:25 +0800

    统计，注册

--- root/action_ins/my_project_zhcx.php
+++ root/action_ins/my_project_zhcx.php
@@ -2,11 +2,12 @@
 	require("../conn.php");
 	$starttimevalue=$_POST["starttimevalue"]; //起始日期
 	$endtimevalue=$_POST["endtimevalue"]; //截止日期
+	$projectId = $_POST['projectId'];
 	
 	$showtime=date("Y-m-d");
 	
 	//待签批  逾期  驳回  归集
-	$sql1 = "select count(*) as sign from table_mes where TabSta='1' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
+	$sql1 = "select count(*) as sign from table_mes where TabSta='1' and ProAId = '".$projectId."' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
 	$result1 = $conn->query($sql1);
 	if($result1->num_rows > 0){
 		while($row = $result1->fetch_assoc()){
@@ -16,7 +17,7 @@
 	
 	
 	//逾期
-	$sql2 = "select count(*) as overdue from table_mes where TabSta='3' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
+	$sql2 = "select count(*) as overdue from table_mes where TabSta='3' and ProAId = '".$projectId."' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
 	$result2 = $conn->query($sql2);
 	if($result2->num_rows > 0){
 		while($row = $result2->fetch_assoc()){
@@ -25,7 +26,7 @@
 	}
 	
 	//驳回
-	$sql3 = "select count(*) as reject from table_mes where TabSta='2' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
+	$sql3 = "select count(*) as reject from table_mes where TabSta='2' and ProAId = '".$projectId."' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
 	$result3 = $conn->query($sql3);
 	if($result3->num_rows > 0){
 		while($row = $result3->fetch_assoc()){
@@ -35,7 +36,7 @@
 	
 	
 	//归集
-	$sql5 = "select count(*) as Imputation from table_mes where TabSta='4' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
+	$sql5 = "select count(*) as Imputation from table_mes where TabSta='4' and ProAId = '".$projectId."' and TabUDa between '$starttimevalue'and '$endtimevalue' ";
 	$result5 = $conn->query($sql5);
 	if($result5->num_rows > 0){
 		while($row = $result5->fetch_assoc()){
